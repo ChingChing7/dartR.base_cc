39a75634eda0bbb1e0d5a856a6ff08ff6ae3d43a
Ching Ching Lau <chingchinglau@Chings-MacBook-Air.local>
2024-08-06 11:23:13 +1000

    exit if not passing the condition

--- R/gl.read.vcf.r
+++ R/gl.read.vcf.r
@@ -27,6 +27,7 @@ gl.read.vcf <- function(vcffile,
                         verbose = NULL, mode=NULL) {
   if(mode!="compressed" | mode!="polyploid"){
     cat(error("  Please choose 'compressed' or 'polyploid' mode \n"))
+    return(-1)
   }
   # SET VERBOSITY
   verbose <- gl.check.verbosity(verbose)
